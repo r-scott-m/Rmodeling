70853fd3224c58700dd341c84401ccf8ed727ce7
Scott McIsaac <rsm@calicolabs.com>
2018-12-16 17:45:25 -0800

    Rmodeling

---
+++ temp.R
@@ -0,0 +1,94 @@
+library(tidyverse)
+library(purrrmodels <- models %>% 
+          mutate(dist = purrr::map2_dbl(a1, a2, sim1_dist))
+        models)
+models <- tibble(
+a1 = runif(250, -20, 40),
+a2 = runif(250, -5, 5))
+library(modelr)
+
+ggplot(sim1, aes(x, y)) + 
+  geom_point()
+
+ggplot(sim1, aes(x, y)) + 
+  geom_abline(aes(intercept = a1, slope = a2), data = models, alpha = 1/4) +
+  geom_point() 
+
+########################
+########################
+
+model1 <- function(a, data) {
+  a[1] + data$x * a[2]
+}
+model1(c(7, 1.5), sim1)
+
+measure_distance <- function(mod, data) {
+  diff <- data$y - model1(mod, data)
+  sqrt(mean(diff ^ 2))
+}
+
+sim1_dist <- function(a1, a2) {
+  measure_distance(c(a1, a2), sim1)
+}
+
+measure_distance(c(7, 1.5), sim1)
+
+
+
+models <- models %>% 
+  mutate(dist = purrr::map2_dbl(a1, a2, sim1_dist))
+models
+
+
+########################
+########################
+
+ggplot(sim1, aes(x, y)) + 
+  geom_point(size = 2, colour = "grey30") + 
+  geom_abline(
+    aes(intercept = a1, slope = a2, colour = -dist), 
+    data = filter(models, rank(dist) <= 10)
+  )
+
+ggplot(sim1, aes(x, y)) + 
+  geom_point(size = 2, colour = "grey30") + 
+  geom_abline(
+    aes(intercept = a1, slope = a2, colour = -dist), 
+    data = filter(models, rank(dist) < 10)
+  )
+
+
+ggplot(models, aes(a1, a2)) +
+  geom_point(data = filter(models, rank(dist) <= 10), size = 4, colour = "red") +
+  geom_point(aes(colour = -dist))
+
+
+
+
+####
+data(iris)
+by_species <- iris %>% 
+  arrange(Species, Sepal.Length) %>% 
+  group_by(Species) %>% 
+  mutate(rank=row_number())
+
+by_species %>% filter(rank <= 3)
+
+
+ggplot(models, aes(a1, a2)) +
+  geom_point(data = filter(models, rank(dist) <= 10), size = 4, colour = "red") +
+  geom_point(aes(colour = -dist))
+
+
+
+
+grid <- expand.grid(
+  a1 = seq(-5, 20, length = 25),
+  a2 = seq(1, 3, length = 25)
+) %>% 
+  mutate(dist = purrr::map2_dbl(a1, a2, sim1_dist))
+
+grid %>% 
+  ggplot(aes(a1, a2)) +
+  geom_point(data = filter(grid, rank(dist) <= 10), size = 4, colour = "red") +
+  geom_point(aes(colour = -dist)) 
